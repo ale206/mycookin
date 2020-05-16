415f06368bd8fc80cc77d27bc6d951cfc2e4bc0b
Alessio Di Salvo <alessio.disalvo@gmail.com>
2020-05-16 13:05:45 +0200

    Changing WeatherForecast controller to HealthCheck. Restore UseHttpsRedirection.

---
+++ API/MyCookin.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyCookin.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"Alive! {DateTime.UtcNow}";
+        }
+    }
+}
