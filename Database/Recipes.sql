5cdfd520b5edb674ff36c2f5931b5b2f08d84261
Alessio Di Salvo <alessio.disalvo@gmail.com>
2020-05-20 17:53:17 +0200

    First Recipes dump file and helper script

---
+++ Database/Recipes.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: Recipes
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Language`
+--
+
+DROP TABLE IF EXISTS `Language`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Language` (
+  `Id` bigint NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Code` varchar(50) NOT NULL,
+  `Enabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Language`
+--
+
+LOCK TABLES `Language` WRITE;
+/*!40000 ALTER TABLE `Language` DISABLE KEYS */;
+INSERT INTO `Language` VALUES (1,'English','En',1),(2,'Italiano','IT',1);
+/*!40000 ALTER TABLE `Language` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-20 18:36:13
---
+++ Database/mycookin-dump-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+# -e means exit if any command fails
+# This script helps to create a dump of the database in case of new changes
+# Just change the credentials and the path to your local git repository and run it with
+# zsh mycookin-dump-db.sh
+DBHOST=localhost
+DBUSER=YOUR_USER
+DBPASS=YOUR_PASSWORD # do this in a more secure fashion
+DBNAME=Recipes
+GITREPO=PATH_TO_YOUR_REPO
+cd $GITREPO
+mysqldump -h $DBHOST -u $DBUSER -p$DBPASS  $DBNAME > $GITREPO/Recipes.sql # if you don't want to include the data, add -d flag before $DBNAME
